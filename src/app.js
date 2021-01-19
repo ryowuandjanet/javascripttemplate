863c64a753ffc29e3d5926b6ddfec9ae74a98f23
ryowuandjanet <ryowudata@gmail.com>
2021-01-19 14:09:27 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import { person, savHello } from './lib';
+
+async function getPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  return data;
+}
+
+getPosts().then(posts => console.log(posts))
---
+++ src/lib.js
@@ -0,0 +1,9 @@
+export const person = {
+  name: 'Ryowu',
+  location: 'Taiwan',
+  age: 18
+}
+
+export function sayHello(name){
+  return `Hello ${name}`
+}
